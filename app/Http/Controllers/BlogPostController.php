348961218dfb0ece394b2baa64ea733188ff2145
w3schools-test <w3schools-test>
2022-07-19 12:25:31 +0100

    initial commit

---
+++ app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class BlogPostController extends Controller
+{
+
+
+public function index()
+{
+        $posts = BlogPost::all(); //fetch all blog posts from DB
+        return view('blog.index', [
+        'posts' => $posts,
+        ]); //returns the view with posts
+}
+public function create()
+{
+        return view('blog.create');
+//show form to create a blog post
+}
+
+        public function store(Request $request)
+        {
+        //store a new post
+        {
+        $newPost = BlogPost::create([
+        'title' => $request->title,
+        'body' => $request->body,
+        'user_id' => 1
+        ]);
+        return redirect('blog/' . $newPost->id);
+        }
+        }
+public function show(BlogPost $blogPost)
+{
+
+        return view('blog.show',[
+                'post' => $blogPost,
+        ]);
+//show a blog post
+}
+
+public function edit(BlogPost $blogPost)
+{
+        return view('blog.edit', [
+                'post' => $blogPost,
+                ]); 
+//show form to edit the post
+}
+
+public function update(Request $request, BlogPost $blogPost)
+{
+$blogPost->update([
+
+'title' => $request->title,
+'body' => $request->body
+]);
+return redirect('blog/' . $blogPost->id);
+}
+
+public function destroy(BlogPost $blogPost)
+{
+$blogPost->delete();
+return redirect('/blog');
+}
+}
+
